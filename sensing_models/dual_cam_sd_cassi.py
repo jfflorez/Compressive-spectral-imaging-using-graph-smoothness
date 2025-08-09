5849fae9de6a7bde8255277f484503d8075c8e7f
florez_j <juan.florez-ospina@psi.ch>
2025-08-09 18:37:08 +0200

    Refactored DualCameraSDCassiModel using object composition with SDCassiModel, added methods to get CASSI matrx and Side Camera matrix. Backwards compatible.

--- sensing_models/dual_cam_sd_cassi.py
+++ sensing_models/dual_cam_sd_cassi.py
@@ -6,12 +6,12 @@ from scipy.sparse import diags
 from scipy.sparse import vstack
 import matplotlib.pyplot as plt
 
-import sd_cassi
+from sd_cassi import SingleDisperserCassiModel
 
 
 class DualCameraSDCassiModel:
     """ creates a dual camera single disperser cassi model, where """
-    def __init__(self,sdcassi_obj,spectral_sen) -> None:
+    def __init__(self,n1,n2,L,mask,disp_dir,spectral_sen) -> None:
         """
             Parameters:
             sdcassi_model: sd_cassi.SingleDisperserCassiModel object, 
@@ -19,26 +19,31 @@ class DualCameraSDCassiModel:
             ss: numpy.ndarray, two dimensional array of shape (K,L), where each row contains the spectral sensitivity of 
             side information camera's channels. 
             For a panchromatic side camera, K=1, and for an RGB camera, K=3.""" 
-
-        self.n1 = sdcassi_obj.n1
-        self.n2 = sdcassi_obj.n2
-        self.L = sdcassi_obj.L
+        self.sdcassi_obj = SingleDisperserCassiModel(n1,n2,L,mask,disp_dir)
+        self.n1 = n1
+        self.n2 = n2
+        self.L = L
         self.K = spectral_sen.shape[0]
-
         self.spectral_sen = spectral_sen
-        if not hasattr(sdcassi_obj,'Hmtx'):
-            raise NameError("Class attribute Hmtx is not defined, i.e., hasattr(sdcassi_obj,'Hmtx') ->  False.")
-        else:
-            self.Hmtx = sdcassi_obj.Hmtx
+
+        if not hasattr(self.sdcassi_obj,'Hmtx'):
+            self.sdcassi_obj.construct_system_mtx()
+            #raise NameError("Class attribute Hmtx is not defined, i.e., hasattr(sdcassi_obj,'Hmtx') ->  False.")
+        #else:
+        #self.Hmtx = self.sdcassi_obj.Hmtx
         
 
-        if not isinstance(sdcassi_obj, sd_cassi.SingleDisperserCassiModel):
-            raise NameError("Invalid camera model. sdcassi_obj must be an instance of the class SingleDisperserCassiModel")
+        #if not isinstance(sdcassi_obj, sd_cassi.SingleDisperserCassiModel):
+        #    raise NameError("Invalid camera model. sdcassi_obj must be an instance of the class SingleDisperserCassiModel")
 
         if not spectral_sen.shape[1] == self.L:
             raise NameError('Inconsistent shape. spectral_sen must be of shape (K, sdcassi_obj.L)')
+    def get_CASSI_mtx(self):
+        if not hasattr(self.sdcassi_obj,'Hmtx'):
+            return None        
+        return self.sdcassi_obj.Hmtx
 
-    def construct_system_mtx(self):
+    def construct_side_system_mtx(self):
         n1n2 = self.n1*self.n2
         for k in range(self.spectral_sen.shape[0]):
             if k > 0:
@@ -46,19 +51,28 @@ class DualCameraSDCassiModel:
 
             else:                
                 self.Rmtx = kron(self.spectral_sen[k,:], diags(np.ones((n1n2,1)).squeeze(), 0, shape = (n1n2,n1n2)))
-
+    
+    def get_side_camera_mtx(self):
+        if not hasattr(self,'Rmtx'):
+            return None
+        return self.Rmtx
+    
     def take_simulated_snapshots(self,X,SNR1,SNR2):
 
         if not check_shape(X, self.n1, self.n2, self.L):
             raise NameError('Invalid shape. X must be a 3 dimensional array of shape (n1,n2,L).')
 
+        if not hasattr(self.sdcassi_obj,'Hmtx'):
+            self.sdcassi_obj.construct_system_mtx()
+            print('CASSI System matrix Hmtx has been successfully created and added to models attributes.')
+
         if not hasattr(self,'Rmtx'):
-            self.construct_system_mtx()
-            print('System matrix Rmtx has been successfully created and added to models attributes.')
+            self.construct_side_system_mtx()
+            print('Side Camera System matrix Rmtx has been successfully created and added to models attributes.')
 
         n = self.n1*self.n2*self.L
-        m = self.n1*(self.n2 + self.L - 1)
-        y = self.Hmtx*np.reshape(X,(n,1),'F')
+        m = self.n1*(self.n2 + self.L - 1)        
+        y = self.sdcassi_obj.Hmtx*np.reshape(X,(n,1),'F')
         z = self.Rmtx*np.reshape(X,(n,1),'F')
         
         if SNR1 != np.inf:
@@ -98,36 +112,37 @@ def check_shape(A: np.ndarray, height: int, width: int, depth:int) -> bool:
         else:
             return False
 
-
-dataset_dir = 'C:\\Users\\juanf\\OneDrive\\Documents\\GitHub\\Compressive-spectral-imaging-using-graph-smoothness\\Datasets\\'
-dataset = scipy.io.loadmat(dataset_dir + 'simulated_data_HSDC1_DB_Oct092019_5_OE.mat')
-#dataset = scipy.io.loadmat(dataset_dir + 'real_data_SCN_2_scale_2_June032021_OE')
-print(dataset.keys())
-
-
-L  = np.max(dataset['lambda_calib'].shape)
-n1 = dataset['Y'].shape[0]
-n2 = dataset['Y'].shape[1] - L + 1
-
-if 'X' in dataset:
-    x = np.reshape(dataset['X'],(n1*n2*L,1),'F')
-else:
-    x = np.zeros((n1,n2,L)); 
-    x[:,:,L-1] = 1; 
-    x = np.reshape(x,(n1*n2*L,1),'F')
-
-mask = dataset['mask']
-disp_dir = 'left2right'
-#disp_dir = 'right2left'
-sdcassi_model = sd_cassi.SingleDisperserCassiModel(n1,n2,L,mask,disp_dir)
-sdcassi_model.construct_system_mtx()
-
-dc_sdcassi_model = DualCameraSDCassiModel(sdcassi_model,np.ones((1,L)))
+if __name__ == '__main__':
+    dataset_dir = 'Datasets/'
+    dataset = scipy.io.loadmat(dataset_dir + 'simulated_data_HSDC1_DB_Oct092019_5_OE.mat')
+    #dataset = scipy.io.loadmat(dataset_dir + 'real_data_SCN_2_scale_2_June032021_OE')
+    print(dataset.keys())
 
 
-dc_sdcassi_model.take_simulated_snapshots(dataset['X'],SNR1=20, SNR2=np.inf)
-#Y = sdcassi_model.Y
-#Hmtx = sdcassi_cam.construct_cassi_mtx()
+    L  = np.max(dataset['lambda_calib'].shape)
+    n1 = dataset['Y'].shape[0]
+    n2 = dataset['Y'].shape[1] - L + 1
 
-plt.imshow(dc_sdcassi_model.Y)
-plt.imshow(dc_sdcassi_model.Z)
+    if 'X' in dataset:
+        x = np.reshape(dataset['X'],(n1*n2*L,1),'F')
+    else:
+        x = np.zeros((n1,n2,L)); 
+        x[:,:,L-1] = 1; 
+        x = np.reshape(x,(n1*n2*L,1),'F')
+
+    mask = dataset['mask']
+    disp_dir = 'left2right'
+    #disp_dir = 'right2left'
+    #sdcassi_model = sd_cassi.SingleDisperserCassiModel(n1,n2,L,mask,disp_dir)
+    #sdcassi_model.construct_system_mtx()
+
+    dc_sdcassi_model = DualCameraSDCassiModel(n1,n2,L,mask,disp_dir,np.ones((1,L)))
+
+
+    dc_sdcassi_model.take_simulated_snapshots(dataset['X'],SNR1=20, SNR2=np.inf)
+    #Y = sdcassi_model.Y
+    #Hmtx = sdcassi_cam.construct_cassi_mtx()
+
+    plt.imshow(dc_sdcassi_model.Y)
+    plt.figure(), plt.imshow(dc_sdcassi_model.Z)
+    plt.show()
